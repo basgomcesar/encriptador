2cbc774a17443e3a3ffe5c256cc26b888a92fa87
basgomcesar <108818138+basgomcesar@users.noreply.github.com>
2024-02-28 22:44:18 -0600

    animacion vibrar

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
 let textoEncriptador = document.getElementById('mensaje');
 let botonCopiarTexto = document.getElementById('copiar-texto');
 let divEliminar = document.getElementById('texto-eliminar');
+let vibrarDiv = document.getElementById('texto-final');
+
 
 function encriptarTexto() {
     let texto = textoEncriptador.value;
-    let textoEncriptado = '';
-    for (let i = 0; i < texto.length; i++) {
-        if (texto[i] === 'e') {
-            textoEncriptado += 'enter';
-        } else if (texto[i] === 'i') {
-            textoEncriptado += 'imes';
-        } else if (texto[i] === 'o') {
-            textoEncriptado += 'ober';
-        } else if (texto[i] === 'a') {
-            textoEncriptado += 'ai';
-        } else if (texto[i] === 'u') {
-            textoEncriptado += 'ufat';
-        } else {
-            textoEncriptado += texto[i];
-        }
+    if (texto === '') {
+        vibrarElemento();
+    }else {
+        encriptar(texto);
     }
-    textoEncriptador.value = textoEncriptado;
-    mostrarTexto(textoEncriptado);
 }
 function desencriptar() {
     let texto = textoEncriptador.value;
-    let textoDesencriptado = '';
-    textoDesencriptado = texto.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ober', 'o').replaceAll('ai', 'a').replaceAll('ufat', 'u');
-    textoEncriptador.value = textoDesencriptado;
-    mostrarTexto(textoDesencriptado);
+    if (texto === '') {
+        vibrarElemento();
+    }else {
+        desencriptarTexto(texto);
+    }
 }
 function mostrarTexto(texto) {
     divEliminar.innerHTML = '';
@@ -56,4 +46,34 @@ function estiliazarTexto(div) {
     div.style.color = '#495057';
     div.style.backgroundColor = 'white';
     div.readOnly = true;
+}
+function vibrarElemento() {
+    vibrarDiv.classList.add('vibrar');
+    setTimeout(function() {
+        vibrarDiv.classList.remove('vibrar');
+    }, 400);
+}
+function encriptar(texto) {
+    let textoEncriptado = '';
+    for (let i = 0; i < texto.length; i++) {
+        if (texto[i] === 'e') {
+            textoEncriptado += 'enter';
+        } else if (texto[i] === 'i') {
+            textoEncriptado += 'imes';
+        } else if (texto[i] === 'o') {
+            textoEncriptado += 'ober';
+        } else if (texto[i] === 'a') {
+            textoEncriptado += 'ai';
+        } else if (texto[i] === 'u') {
+            textoEncriptado += 'ufat';
+        } else {
+            textoEncriptado += texto[i];
+        }
+    }
+    mostrarTexto(textoEncriptado);
+}
+function desencriptarTexto(texto) {
+    let textoDesencriptado = '';
+    textoDesencriptado = texto.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ober', 'o').replaceAll('ai', 'a').replaceAll('ufat', 'u');
+    mostrarTexto(textoDesencriptado);
 }
